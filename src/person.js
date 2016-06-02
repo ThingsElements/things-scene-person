a33da5a1c483b22d9ff191f6f8e19278419da460
Lee-YuMin <lym@hatiolabs-Mac-mini.local>
2016-06-02 23:54:52 +0900

    사람 3개 추가

--- src/person.js
+++ src/person.js
@@ -1,33 +1,32 @@
-var { Component, Ellipse } = scene
+var { Component, Rect } = scene
 
-export default class Person extends Ellipse {
+export default class Person extends Rect {
 
   _draw(context) {
     var {
-      value = 0,
+      imageNumber = 1,
       hidden = false,
       fillStyle,
-      blankStrokeStyle,
-      cx, cy, rx, ry
+      left,
+      top,
+      width,
+      height
     } = this.model;
 
     if(!hidden){
-      context.translate(cx, cy)
 
       context.beginPath()
 
-      
-      context.ellipse(0, 0, rx, ry, 0, 0, Math.PI * 2)
-
-      context.moveTo(Math.cos(-rx), -ry / 2.5)
-      context.lineTo(rx, ry)
-
+      context.rect(left, top, width, height)
+      this.model.fillStyle = {
+        type: 'pattern',
+        fitPattern: true,
+        image: '../images/' + imageNumber + '.png'
+      }
       this.drawFill(context)
-      this.drawStroke(context)
-
-      context.translate(-cx, -cy)
     }
   }
-}
 
+  get controls(){}
+}
 Component.register('person', Person)
