84331b4eb3e9e271359bb395bee3f5f6bfe0311d
Lee-YuMin <lym@hatiolabs-Mac-mini.local>
2016-06-10 16:09:28 +0900

    히든 수정

--- src/person.js
+++ src/person.js
@@ -4,29 +4,25 @@ export default class Person extends Ellipse {
 
   _draw(context) {
     var {
-      hidden = false,
       cx,
       cy,
       rx,
       ry
     } = this.model;
 
-    if(!hidden){
+    context.beginPath()
+    context.ellipse(cx, cy, rx, ry, 0, 0, Math.PI * 2)
+    context.fillStyle = '#39b44a'
+    context.fill()
 
-      context.beginPath()
-      context.ellipse(cx, cy, rx, ry, 0, 0, Math.PI * 2)
-      context.fillStyle = '#39b44a'
-      context.fill()
-
-      context.beginPath()
-      context.ellipse(cx, cy, rx, ry, 0, 0, Math.PI * 2)
-      this.model.fillStyle = {
-        type: 'pattern',
-        fitPattern: true,
-        image: '../images/person.png'
-      }
-      this.drawFill(context)
+    context.beginPath()
+    context.ellipse(cx, cy, rx, ry, 0, 0, Math.PI * 2)
+    this.model.fillStyle = {
+      type: 'pattern',
+      fitPattern: true,
+      image: '../images/person.png'
     }
+    this.drawFill(context)
   }
 }
 Component.register('person', Person)
